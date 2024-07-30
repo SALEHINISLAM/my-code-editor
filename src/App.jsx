58af3d39a6a02786f84eae3930eea8ef0b2df53d
SALEHIN <msionlinekingdom@gmail.com>
2024-07-30 19:25:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import {Controlled as ControllEditor, Controlled} from 'react-codemirror2'
+import "./App.css";
+import { useState } from 'react'
+import Iframe from 'react-iframe'
+
+function App() {
+  const [code, setCode]=useState('');
+  const run=()=>{
+    let htmlCode=document.getElementById('html-code').value;
+    let cssCode=document.getElementById('css-code').value;
+    let jsCode=document.getElementById('js-code').value;
+    document.getElementById('output').innerHTML=`<html>
+          <body>${htmlCode}</body>
+          <style>${cssCode}</style>
+          <script>${jsCode}</script>
+        </html>
+    `; 
+  }
+  return (
+    <>
+      <div className="grid grid-cols-1 w-screen min-h-screen">
+        <div className="card bg-base-300 rounded-box grid  flex-grow place-items-center">
+          <div className="grid md:grid-cols-3 w-full grid-cols-1 h-full">
+            <div className="card bg-base-300 rounded-box grid  place-items-center border p-1">
+              <textarea
+                className="textarea textarea-bordered mockup-code w-full h-full rounded-box overflow-y-auto text-base-content text-lg"
+                placeholder="Wite HTML Code here" id="html-code"
+              ></textarea>
+            </div>
+
+            <div className="card bg-base-300 rounded-box grid  place-items-center border">
+              <textarea
+                className="textarea textarea-bordered mockup-code w-full h-full rounded-box overflow-y-auto text-base-content text-lg"
+                placeholder="Wite CSS Code here" id="css-code"
+              ></textarea>
+            </div>
+
+            <div className="card bg-base-300 rounded-box grid place-items-center border">
+              <textarea
+                className="textarea textarea-bordered mockup-code w-full h-full rounded-box overflow-y-auto text-base-content text-lg"
+                placeholder="Wite JavaScript Code here" id="js-code"
+              >bro This is not functional yet</textarea>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-300 rounded-box  flex-grow  ">
+          <button className="btn" onClick={run}>
+            Run Code
+          </button>
+          <div className="mockup-browser h-full w-full overflow-auto">
+            <div className="w-full h-full" id='output'></div>
+          </div>
+          </div>
+        </div>
+      
+    </>
+  );
+}
+
+export default App;
